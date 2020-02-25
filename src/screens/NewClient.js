193e69dcf96e7a509976497c0614e90d3b27a4c1
iuliamidus <61030305+iuliamidus@users.noreply.github.com>
2020-02-25 13:39:47 +0000

    update

--- src/screens/NewClient.js
+++ src/screens/NewClient.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { Container, Header, Left, Button, Icon, Body, Title, Content, Form, Item, Input, Label } from 'native-base';
+import { Container, Header, Left, Button, Icon, Body, Title, Content, Form, Item, Input, Label, Text } from 'native-base';
 import {Image, StatusBar} from 'react-native';
 import { color } from 'react-native-reanimated';
 import CSHeader from '../components/CSHeader';
@@ -32,10 +32,14 @@ export default class NewClient extends Component {
               <Label>Phone number</Label>
               <Input />
             </Item>
-            <Item regular style = {marginTop=60}>
+            <Item regular style = {marginTop=60, marginBottom=60}>
             <Input placeholder='Additional information' />
           </Item>
           </Form>
+
+          <Button success style={{marginTop: 30, width: 120, marginLeft: 250}}>
+            <Text>Save</Text>
+          </Button>
         </Content>
       </Container>
     );
